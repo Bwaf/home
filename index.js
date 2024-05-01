90ad068b73bf23c392e74f724a7cda3b99bcd1d6
Fish <115324554+Bwaf@users.noreply.github.com>
2024-05-01 11:04:02 -0700

    added another webpage

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ if (url.searchParams.has('fish')) {
 window.location.href("https://bwaf.github.io/testing/search/fish");
 }
 }
+
+
+//function getQueryParams(){
+  //  try{
+    //    url = window.location.href;
+      //  query_str = url.substr(url.indexOf('?')+1, url.length-1);
+        //r_params = query_str.split('&');
+        //params = {}
+        //for( i in r_params){
+          //  param = r_params[i].split('=');
+            //params[ param[0] ] = param[1];
+        //}
+       // return params;
+    //}
+    //catch(e){
+     //  return {};
+    //}
+//}
