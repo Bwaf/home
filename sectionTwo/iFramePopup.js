b755151e587d552cfff263039048a08b0a638f2a
Fish <115324554+Bwaf@users.noreply.github.com>
2024-05-07 15:36:26 -0700

    added some changes

--- sectionTwo/iFramePopup.js
+++ sectionTwo/iFramePopup.js
@@ -1,5 +1,15 @@
-var iframe=document.getElementById("iFrame");
+var button = document.getElementById('button'); // Assumes element with id='button'
 
-function iframeChanger() {
-    document.getElementById('iFrame');
+button.onclick = function() {
+    var div = document.getElementById('gameDiv');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+    }
+};
+
+function changeSrc(loc) {
+    document.getElementById('iframeId').src = loc;
 }
