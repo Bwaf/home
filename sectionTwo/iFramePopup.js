9748d979c6c34b8f65a44a7d01874d0c766032eb
Fish <115324554+Bwaf@users.noreply.github.com>
2024-05-08 10:25:55 -0700

    fixed a problem with the code

--- sectionTwo/iFramePopup.js
+++ sectionTwo/iFramePopup.js
@@ -14,3 +14,20 @@ button.onclick = function() {
 function changeSrc(loc) {
     document.getElementById('iframeId').src = loc;
 }
+
+
+var portfolioDiv = document.getElementById('portfolio');
+var resultsDiv = document.getElementById('results');
+
+var portfolioBtn = document.getElementById('RenderPortfolio_Btn');
+var resultsBtn = document.getElementById('RenderResults_Btn');
+
+portfolioBtn.onclick = function() {
+    resultsDiv.setAttribute('class', 'hidden');
+    portfolioDiv.setAttribute('class', 'visible');
+};
+
+resultsBtn.onclick = function() {
+    portfolioDiv.setAttribute('class', 'hidden');
+    resultsDiv.setAttribute('class', 'visible');
+};
