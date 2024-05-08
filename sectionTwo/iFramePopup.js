e4ee0518444d71ad890094c227f51c23eae70b0d
Fish <115324554+Bwaf@users.noreply.github.com>
2024-05-08 08:27:01 -0700

    added some punctuational changes

--- sectionTwo/iFramePopup.js
+++ sectionTwo/iFramePopup.js
@@ -1,22 +1,12 @@
 var button = document.getElementById('button'); // Assumes element with id='button'
 
 button.onclick = function() {
-    var div = document.getElementById('gameDiv');
+    var div = document.getElementById('div');
     if (div.style.display !== 'none') {
         div.style.display = 'none';
     }
     else {
         div.style.display = 'block';
-    }
-};
-
-buttontwo.onclick = function() {
-    var div = document.getElementById('gameDiv');
-    if (div.style.display == 'none') {
-        div.style.display = 'block';
-    }
-    else {
-        div.style.display = 'none';
     }
 };
 
