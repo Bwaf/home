8b43d0efa33990d649faa2f180a428cc4c6d35a9
Fish <115324554+Bwaf@users.noreply.github.com>
2024-09-15 19:30:49 -0700

    coolio

--- myGames/pacman/script.js
+++ myGames/pacman/script.js
@@ -1,3 +1,11 @@
+/*
 var canvas = document.getElementById('canvasSpace');
 var ctx = canvas.getContext("2d");
+ctx.fillText("Hello, World!!!",10,150);
+*/
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.height = 400;
+canvas.width = 600;
+document.body.appendChild(canvas);
 ctx.fillText("Hello, World!!!",10,150);
