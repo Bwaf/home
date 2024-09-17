4dd1be9ca91c6620afdbe1bd5899f8d7336430f5
Fish <115324554+Bwaf@users.noreply.github.com>
2024-09-16 20:53:22 -0700

    730

--- myGames/pacman/script.js
+++ myGames/pacman/script.js
@@ -28,7 +28,7 @@ function render() {
     context.fillStyle = "blue";
     context.fillRect(0,0,canvas.width, canvas.height);
     //this is the original numbers = (320 left and right,0 up and down, 32,32,0,0,(50,50)these two are to resize)
-    context.drawImage(mainImage, 750,0,32,32,0,10,50,50);
+    context.drawImage(mainImage, 730,0,32,32,0,10,50,50);
 }
 
 
